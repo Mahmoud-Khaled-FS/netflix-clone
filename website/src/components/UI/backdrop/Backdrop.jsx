bcebadea548d5c523edc69d35302002fd798b5d1
Mahmoud-Khaled-FS <mahmoudkhaled1215@gmail.com>
2022-07-23 20:03:19 +0200

    mahmoud

--- website/src/components/UI/backdrop/Backdrop.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import classes from './styles/backdrop.module.css';
-function Backdrop(props) {
-  return <div {...props} className={classes.backdrop}></div>;
-}
-
-export default Backdrop;
